5a4eb325adfd046b2e32686d51607154c1b9d6e7
omatveyuk <oxana@mishina.com>
2015-05-16 19:30:06 +0000

    Add user microposts - AMAZON

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,9 +3,9 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY']
+      :aws_access_key_id     => ENV['AKIAJPV2WQ4SQUR4JX7Q '],
+      :aws_secret_access_key => ENV['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']
     }
-    config.fog_directory     =  ENV['S3_BUCKET']
+    config.fog_directory     =  ENV['omatveyuk']
   end
 end
